c6525bfa09fb3af149ee18a3f7783c4db49cb66b
Eduardo Moreira <contato@eduardoem.com.br>
2020-03-02 13:42:52 -0300

    Add state to quizzes context

--- src/contexts/quizzes.js
+++ src/contexts/quizzes.js
@@ -1,10 +1,14 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const QuizzesContext = createContext();
 
 export const QuizzesProvider = ({ children }) => {
+  const [ quizzes, setQuizzes ] = useState([]);
+
+  const store = { quizzes }
+
   return (
-    <QuizzesContext.Provider value={[]}>
+    <QuizzesContext.Provider value={store}>
       { children }
     </QuizzesContext.Provider>
   )
